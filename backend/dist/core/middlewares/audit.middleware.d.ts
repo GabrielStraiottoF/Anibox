a659b158992e1febe587de2d36ef5cae8f808786
GabrielStraiottoF <gabrielstraiotto7@gmail.com>
2026-07-25 12:32:14 -0300

    Remove node_modules and build artifacts from repository

--- backend/dist/core/middlewares/audit.middleware.d.ts
+++
@@ -1,7 +0,0 @@
-import { NestMiddleware } from '@nestjs/common';
-import { Request, Response, NextFunction } from 'express';
-export declare class AuditMiddleware implements NestMiddleware {
-    private readonly logger;
-    constructor();
-    use(req: Request, res: Response, next: NextFunction): void;
-}
