f45aab79dd7ac8e14940c0ebe61c7d30d31d114a
GabrielStraiottoF <gabrielstraiotto7@gmail.com>
2026-09-05 08:37:14 -0300

    first commit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,18 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
-import { HealthController } from './health.controller';
+import { AuthModule } from './core/auth/auth.module';
+import { MediaController } from './core/controllers/media.controller';
 import { AuditMiddleware } from './core/middlewares/audit.middleware';
 import { ContentProviderService } from './core/services/content-provider.service';
+import { PrismaModule } from './core/prisma/prisma.module';
+import { HealthController } from './health.controller';
 
 @Module({
-  imports: [],
-  controllers: [HealthController],
-  providers: [ContentProviderService],
+  imports: [PrismaModule, AuthModule],
+  controllers: [HealthController, MediaController],
+  providers: [ContentProviderService, AuditMiddleware],
 })
 export class AppModule {
-  configure(consumer: MiddlewareConsumer) {
+  configure(consumer: MiddlewareConsumer): void {
     consumer.apply(AuditMiddleware).forRoutes('*');
   }
 }
